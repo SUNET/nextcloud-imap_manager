c87a55157896be567f4c1417524dbd1b5d5a3c6e
Micke Nordin <kano@sunet.se>
2026-04-17 10:10:54 +0200

    style: add license header to adminSettings.js

--- src/adminSettings.js
+++ src/adminSettings.js
@@ -1,3 +1,11 @@
+/**
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Micke Nordin <kano@sunet.se>
+ * @copyright Micke Nordin 2025
+ */
+
 import { createApp } from 'vue'
 import AdminSettings from './components/AdminSettings.vue'
 
